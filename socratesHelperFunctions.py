5e2558fc562a5501926b76ccfe4d70e22bee90b2
Elliott Rose <esteverose@gmail.com>
2020-05-05 16:12:39 -0700

    add Socrates helper functions for creating new voice files. Add Final Report

---
+++ socratesHelperFunctions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 21:14:51 2020
+
+@author: elliott
+"""
+
+# def sfEMSProtocols():# to organize protocols into text files for Socrates
+#     path = 'EMS_ProtocolManual_020320.pdf'
+#     protocol = "1.1" 
+#     with open(path, 'rb') as f:
+#         pdf = PdfFileReader(f)
+#         for i in range(5,pdf.getNumPages()):
+# #            I'm skipping the first 5 pages as they are a
+# #            glossary and updates to the last manual
+#             page = pdf.getPage(i) 
+#             text = page.extractText()
+#             pNum = text[0:6]
+#             print('just indexed', pNum)
+#             if protocol in pNum:
+#                 f = open(pNum+".txt", "a")
+#                 f.write(text)
+#                 f.close()
+#             else:
+#                 protocol = pNum
+#                 f = open(pNum+".txt", "a")
+#                 f.write(text)
+#                 f.close()
+                
+            
